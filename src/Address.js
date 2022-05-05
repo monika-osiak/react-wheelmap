64e06e95b3bcfec52fd9914c4301cbd989506843
Monika Osiak <m.osiak46@gmail.com>
2022-05-05 13:59:38 +0200

    Prettify address from API

--- src/Address.js
+++ src/Address.js
@@ -1,6 +1,15 @@
 import { useEffect } from "react";
 import useFetch from "./useFetch";
 
+const pretifyAddress = (address) => {
+  let result = address.road + " "
+  result = result + address.house_number + ", "
+  result = result + address.postcode + " "
+  result = result + address.city + ", "
+  result = result + address.country
+  return result
+}
+
 const Address = ({position}) => {
     let url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + position.lat+ "&lon=" + position.lng
     const { error, isPending, data } = useFetch(url)
@@ -9,7 +18,7 @@ const Address = ({position}) => {
         <div class="address">
           { error && <div>{ error }</div> }
           { isPending && <div>Loading...</div> }
-          { data && <p>{data.display_name}</p> }
+          { data && <p>{pretifyAddress(data.address)}</p> }
         </div>
       );
 }
