e06d0da9e359e25382c82aa9c853cea734cf276b
Monika Osiak <m.osiak46@gmail.com>
2022-05-07 01:34:05 +0200

    Change the color of a place marker to gold

--- src/PlaceIcon.js
+++ src/PlaceIcon.js
@@ -2,7 +2,7 @@ import L from 'leaflet'
 
 const PlaceIcon = () => {
     return L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
