c2e991a857142cfa8d38cefa63f8d072564d2512
Monika Osiak <m.osiak46@gmail.com>
2022-05-12 12:29:29 +0200

    Fix result list

--- src/SearchField.js
+++ src/SearchField.js
@@ -2,42 +2,50 @@ import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import SearchResultIcon from './SearchResultIcon';
-import { Link } from 'react-router-dom';
 import L from 'leaflet';
 
 const SearchField = () => {
   const provider = new OpenStreetMapProvider(); 
-  // const handleLocation = (label, position) => {
-  //   return `<div className="location">
-  //     <p>${label}</p>
-  //     <a href=/points?lat=${position.lat}&lng=${position.lng}><button>Dodaj punkt</button><a>
-  //     <a href=/places?lat=${position.lat}&lng=${position.lng}><button>Dodaj miejsce</button><a>
-  //   </div>`
-  // }
+  const handleLocation = (label, position) => {
+    return `<div className="location">
+      <p>${label}</p>
+      <a href=/points?lat=${position.lat}&lng=${position.lng}><button>Dodaj punkt</button><a>
+      <a href=/places?lat=${position.lat}&lng=${position.lng}><button>Dodaj miejsce</button><a>
+    </div>`
+  }
   const searchControl = new GeoSearchControl({
     provider: provider,
     position: 'topright',
-    showPopup: true,
+    showPopup: false,
     autoComplete: true, // optional: true|false  - default true
-    autoCompleteDelay: 1,
-    showMarker: true,
+    autoCompleteDelay: 250,
+    showMarker: false,
     marker: {
       icon: SearchResultIcon(),
       draggable: false,
     },
-    //popupFormat: ({ query, result }) => handleLocation(result.label, { lat: result.y, lng: result.x }),
+    popupFormat: ({ query, result }) => handleLocation(result.label, { lat: result.y, lng: result.x }),
+    maxMarker: 1,
+    autoClose: true,
+    retainZoomLevel: true,
+    maxSuggestions: 5,
+    keepResult: false,
+    resultFormat: function(t) {
+      return "" + t.result.label;
+    },
+    updateMap: !0
   });
 
   const map = useMap();
-  // const yourEventHandler = (e) => {
-  //   console.log(e.location);
-  //   var popup = L.popup()
-  //     .setLatLng([e.location.y, e.location.x])
-  //     .setContent(handleLocation(e.location.label, { lat: e.location.y, lng: e.location.x }))
-  //     .openOn(map);
-  // }
+  const yourEventHandler = (e) => {
+    console.log(e.location);
+    var popup = L.popup()
+      .setLatLng([e.location.y, e.location.x])
+      .setContent(handleLocation(e.location.label, { lat: e.location.y, lng: e.location.x }))
+      .openOn(map);
+  }
 
-  // map.on('geosearch/showlocation', yourEventHandler);
+  map.on('geosearch/showlocation', yourEventHandler);
 
   useEffect(() => {
     map.addControl(searchControl);
