007a6b8daa043eacfc87c63745208ef26134e75a
Monika Osiak <m.osiak46@gmail.com>
2022-05-19 14:13:12 +0200

    Change name in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>React Wheelmap</h1>
+      <h1>Wheelmap 2.0</h1>
       <div className="links">
         <Link to="/">Mapa</Link>
         <Link to="/about">O aplikacji</Link>
